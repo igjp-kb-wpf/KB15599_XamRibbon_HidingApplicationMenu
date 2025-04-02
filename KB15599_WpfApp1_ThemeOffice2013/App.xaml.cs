3b843134b0202632a4ba87bfc3798a9e8b6f8ca5
norikois <33947244+norikois@users.noreply.github.com>
2025-04-02 16:46:21 +0900

    Initial code commit

---
+++ KB15599_WpfApp1_ThemeOffice2013/App.xaml.cs
@@ -0,0 +1,14 @@
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KB15599_WpfApp1_ThemeOffice2013
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
